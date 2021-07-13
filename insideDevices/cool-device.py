bb2827a057911f5bcf1e2003daedfbf6d8ae59fb
Leon <leon.perianu95@e-uvt.ro>
2021-07-13 13:18:09 +0300

    Final version

--- insideDevices/cool-device.py
+++ insideDevices/cool-device.py
@@ -9,7 +9,7 @@ state = 0
 def getIncomingCommands():
     global state
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(("0.0.0.0", 5006))
     while True:
         if connection.CONNECTED:
